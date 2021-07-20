22af366c981ce84a8d1543e327702838fe958716
Diego Gomes <dsgomes1990@gmail.com>
2021-07-19 22:30:31 -0300

    Initial structure

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: 'authtdd',
+  password: 'pin123',
+  database: 'nodeauth',
+  dialect: 'postgres',
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  },
+};
