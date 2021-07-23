bff5557b2e143f5cdf3a4cc62866036777e20251
Diego Gomes <dsgomes1990@gmail.com>
2021-07-22 23:08:50 -0300

    Configuring tests suit

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,5 +1,13 @@
+const { User } = require('../../src/app/models');
+
 describe('Authentication', () => {
-    it('', () => {
-        
+    it('Should create User when given right parameters', async() => {
+        const user = await User.create({
+            name: 'Nome para teste',
+            email: 'nometeste@outlook.com',
+            password_hash: '123456'
+        });
+
+        expect(user.email).toBe('nometeste@outlook.com');
     });
 })
